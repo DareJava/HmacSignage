1053fca670518471c8b5725677e65418014ad09d
DareJava <fatimehindare@gmail.com>
2016-12-27 11:09:25 +0200

    Update HmacSignerExample.java

--- src/com/amacodecode/annotation/main/HmacSignerExample.java
+++ src/com/amacodecode/annotation/main/HmacSignerExample.java
@@ -8,7 +8,7 @@ public class HmacSignerExample {
 
 	public static void main(String[] args) throws Exception {
 		NotificationClassLevel n = new NotificationClassLevel(1L, "sd", "af");
-		String testKey = "38D2ADECE6203DF2DABFA68D87C55CAE0BC030876120E0505B0C775C88323D0F";
+		String testKey = "zsdgzgfdhdhgfh545754757547457457sdhdhfggjgjgfjgfjgfjgj";
 		NotificationFieldLevel b = new NotificationFieldLevel(1L, "sd", "af");
 		ObjectHmacSigner s = new ObjectHmacSigner();
 		System.out.print(s.generateMac(testKey, ":", n));
